46df3b85501488c6ca53332928792d320ea22b4f
dark <darkhunter0@web.de>
2021-12-02 21:10:52 +0100

    Day 1

---
+++ SonarSweep/SonarSweep.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace AdventOfCodeAdventure.SonarSweep
+{
+    public class SonarSweep
+    {
+        static void Main(string[] args)
+        {
+            SonarSweep sonarSweep = new SonarSweep();
+            sonarSweep.ScanFile();
+        }
+
+        private void ScanFile()
+        {
+            string filePath = "D:/Repository/AdventOfCodeAdventure/SonarSweep/SonarSweepLines.txt";
+            Console.WriteLine(filePath);
+            
+            if (File.Exists(filePath))
+            {
+                int measurements = 0;
+                int currentHigh = 0;
+                foreach (string currentLine in File.ReadLines(filePath))
+                {
+                    int newHight = Int32.Parse(currentLine);
+
+                    if (currentHigh < newHight)
+                    {
+                        measurements++;
+                    }
+                    currentHigh = newHight;
+                }
+                Console.WriteLine(measurements - 1); // Remove the first comparison of "0 vs First File Line"
+            }
+            else
+            {
+                Console.WriteLine("File not Found!");
+            }
+        }
+    }
+}
