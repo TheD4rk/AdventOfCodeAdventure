537d959710aecc8e802ae66c4e6b12d83f709261
Dark <isaaklux@hotmail.com>
2023-03-21 21:59:37 +0100

    Day 01

---
+++ Day_01/GluttonousElf.cs
@@ -0,0 +1,57 @@
+﻿
+using System;
+
+namespace AdventOfCodeAdventure.Day_01
+{
+    public class GluttonousElf
+    {
+        private readonly string _filePath = "D:/Repositories/AdventOfCodeAdventure/Day_01/Calories.txt";
+        private int[] _topElves = {0,0,0};
+
+        public int FindMostCalories()
+        {
+            int currentMax = 0;
+            int currentSum = 0;
+
+            foreach (string currentLine in System.IO.File.ReadLines(_filePath))
+            {
+                if (currentLine.Length == 0)
+                {
+                    if (currentSum > currentMax) currentMax = currentSum;
+                    CheckForTop3(currentSum);
+                    currentSum = 0;
+                }
+                else
+                {
+                    currentSum += Int32.Parse(currentLine);
+                }
+            }
+
+            int top3Sum = _topElves[0] + _topElves[1] + _topElves[2];
+            System.Console.WriteLine("Top 3 Elves have: " + top3Sum + " Calories.");
+
+            return currentMax;
+        }
+
+        private void CheckForTop3(int newElf)
+        {
+            if (newElf < _topElves[0]) return;
+            if (newElf < _topElves[1])
+            {
+                _topElves[0] = newElf;
+                return;
+            }
+
+            if (newElf < _topElves[2])
+            {
+                _topElves[0] = _topElves[1];
+                _topElves[1] = newElf;
+                return;
+            }
+
+            _topElves[0] = _topElves[1];
+            _topElves[1] = _topElves[2];
+            _topElves[2] = newElf;
+        }
+    }
+}
--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,10 +1,15 @@
 ﻿
+using AdventOfCodeAdventure.Day_01;
+
 namespace AdventOfCodeAdventure
 {
     public class EntryPoint
     {
         static void Main(string[] args)
         {
+            // Day 01 - Find Elf with most Calories
+            GluttonousElf gluttonousElf = new GluttonousElf();
+            System.Console.WriteLine(gluttonousElf.FindMostCalories());
         }
     }
 }
