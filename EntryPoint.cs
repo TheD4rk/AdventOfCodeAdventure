97f2a74927b634c99728c546b8e0684c99632a4f
Dark <isaaklux@hotmail.com>
2023-03-21 20:06:09 +0100

    Reset

--- EntryPoint.cs
+++ EntryPoint.cs
@@ -1,18 +1,10 @@
-﻿using AdventOfCodeAdventure.Day01;
-
+﻿
 namespace AdventOfCodeAdventure
 {
     public class EntryPoint
     {
         static void Main(string[] args)
         {
-            //  -- Day 01 - Part 01 --
-            //SonarSweep sonarSweep = new SonarSweep();
-            //sonarSweep.ScanFile();
-            
-            // -- Day 01 - Part 02 --
-            SumSonar sumSonar = new SumSonar();
-            sumSonar.CalculateSums();
         }
     }
 }
